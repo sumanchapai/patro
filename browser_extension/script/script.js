ca4f5f0c98b7a2829366239f38617a7c9ffa72b7
Supreme Paudel <supremepaudel49@gmail.com>
2023-11-26 20:08:50 -0600

    Change variables names to camelCase

--- browser_extension/script/script.js
+++ browser_extension/script/script.js
@@ -1,14 +1,14 @@
-var months_in_eng = ["baishakh", "jestha", "asar", "shrawan",
+var monthsInEng = ["baishakh", "jestha", "asar", "shrawan",
         "bhadau", "aswin", "kartik", "mansir", 
         "poush", "magh", "falgun", "chaitra"];
 
 var weekNamesArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var current_date_td_element = null;
-var current_selected_td_element = null;
-var current_selected_td_element_id = null;
+var currentDateTdElement = null;
+var currentSelectedTdElement = null;
+var currentSelectedTdElementID = null;
 
-var index_to_month = {
+var indexToMonth = {
     0:'Baishakh',
     1:"Jestha",
     2:"Asar", 
@@ -23,7 +23,7 @@ var index_to_month = {
     11:"Chaitra"
 }
 
-var month_to_index= {
+var monthToIndex= {
     'Baishakh':0,
     "Jestha":  1,
     "Asar":    2,
@@ -49,7 +49,7 @@ function capitalizeString(string_){
     return string_[0].toUpperCase() + string_.slice(1);
 }
 
-function reset_td_values(){
+function resetTdValues(){
     let tbody = document.querySelector("tbody");
     for (let td of tbody.querySelectorAll("td")){
         td.innerText = "";
@@ -58,38 +58,36 @@ function reset_td_values(){
 }
 
 function populateTrValues(monthValue){
-    reset_td_values();
-    let tbody_element = document.querySelector("tbody");
-    let all_td_elements = tbody_element.querySelectorAll("td");
-    var current_day_index = 0
+    resetTdValues();
+    let tbodyElement = document.querySelector("tbody");
+    let allTdElements = tbodyElement.querySelectorAll("td");
+    var currentDayIndex = 0
     
-    let starting_week_index = 0;
-    let all_days = yearData["months"][index_to_month[monthValue]]["days"];
+    let startingWeekIndex = 0;
+    let allDays = yearData["months"][indexToMonth[monthValue]]["days"];
 
-    let all_th_elements = document.querySelectorAll("th");
-    for (let th_element of all_th_elements){
-        if (all_th_elements[starting_week_index].id.toLowerCase() == all_days[current_day_index]["eng_weekday"].toLowerCase()){
+    let allThElements = document.querySelectorAll("th");
+    for (let thElement of allThElements){
+        if (allThElements[startingWeekIndex].id.toLowerCase() == allDays[currentDayIndex]["eng_weekday"].toLowerCase()){
             break;
     }
         else{
-            starting_week_index++;}
+            startingWeekIndex++;}
     }
 
-    all_td_elements = [...all_td_elements];
-    all_td_elements = all_td_elements.slice(starting_week_index);
+    allTdElements = [...allTdElements];
+    allTdElements = allTdElements.slice(startingWeekIndex);
 
-    for (let td_element of all_td_elements){
-        td_element.innerText = all_days[current_day_index]["nep"]
+    for (let tdElement of allTdElements){
+        tdElement.innerText = allDays[currentDayIndex]["nep"]
         
-//        let date_list = all_days[current_day_index]["nep_date"].split('-');
- //       let nepali_date_in_english = date_list[date_list.length -1];
-        td_element.setAttribute("id",all_days[current_day_index]["nep_date"]);
-        current_day_index++;
-        if (all_days.length == current_day_index){break;}
+        tdElement.setAttribute("id",allDays[currentDayIndex]["nep_date"]);
+        currentDayIndex++;
+        if (allDays.length == currentDayIndex){break;}
     }
 }
 
-async function showCalendar(monthValue, yearValue, first_time = false){
+async function showCalendar(monthValue, yearValue, firstTime = false){
 
     if (!(ALL_YEARS_DATA.hasOwnProperty(yearValue))) {
         let response = await fetch(API_URL+`${yearValue}.json`)
@@ -98,38 +96,38 @@ async function showCalendar(monthValue, yearValue, first_time = false){
         yearData = response;
         ALL_YEARS_DATA[yearValue] = response
 
-        showCalendar(monthValue, yearValue, first_time)
+        showCalendar(monthValue, yearValue, firstTime)
 
     } else {
 
         var response = ALL_YEARS_DATA[yearValue]
         yearData = response;
         populateTrValues(monthValue);
-        highlightCurrentDay(first_time);
+        highlightCurrentDay(firstTime);
         highlightSelectedDay();
 }
 }
 function highlightSelectedDay(){
-    if (current_selected_td_element){
-        if (current_selected_td_element_id != `${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`){
-            current_selected_td_element.removeAttribute("style");
+    if (currentSelectedTdElement){
+        if (currentSelectedTdElementID != `${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`){
+            currentSelectedTdElement.removeAttribute("style");
         }
     }
 
-    let needed_element = document.getElementById(current_selected_td_element_id);
-    if (needed_element && needed_element != current_date_td_element){
-        needed_element.setAttribute("style", "border-radius: 50%;background-color:orange;line-height: 100%;text-align: center;");
+    let neededElement = document.getElementById(currentSelectedTdElementID);
+    if (neededElement && neededElement != currentDateTdElement){
+        neededElement.setAttribute("style", "border-radius: 50%;background-color:orange;line-height: 100%;text-align: center;");
     }
 }
-function highlightCurrentDay(first_time){
+function highlightCurrentDay(firstTime){
     try {
-        current_date_td_element = document.getElementById(`${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`)
-        if (current_date_td_element != null)
-        current_date_td_element.setAttribute("style", "border-radius: 50%;background-color:blue;line-height: 100%;text-align: center;");
-        if (first_time){
-            current_selected_td_element = current_date_td_element;
-            current_selected_td_element_id = current_date_td_element.id;
-            displayDayInformation(current_date_td_element);
+        currentDateTdElement = document.getElementById(`${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`)
+        if (currentDateTdElement != null)
+        currentDateTdElement.setAttribute("style", "border-radius: 50%;background-color:blue;line-height: 100%;text-align: center;");
+        if (firstTime){
+            currentSelectedTdElement = currentDateTdElement;
+            currentSelectedTdElementID = currentDateTdElement.id;
+            displayDayInformation(currentDateTdElement);
         }
     } catch (error) {
         console.log(error) ;
@@ -137,106 +135,106 @@ function highlightCurrentDay(first_time){
 }
 function changeMonthAndYear(type){
 
-    let month_element = document.querySelector("#current_month");
-    let month_value = month_element.innerText;
+    let monthElement = document.querySelector("#current_month");
+    let monthValue = monthElement.innerText;
 
-    let year_element = document.querySelector("#current_year");
-    let year_value = parseInt(year_element.innerText);
+    let yearElement = document.querySelector("#current_year");
+    let yearValue = parseInt(yearElement.innerText);
 
     if (type == 'previous'){
-        if (month_value.toLowerCase() == "baishakh"){
-            year_value -= 1;
-            month_value = 'chaitra';
+        if (monthValue.toLowerCase() == "baishakh"){
+            yearValue -= 1;
+            monthValue = 'chaitra';
         } else {
-        let index = months_in_eng.indexOf(month_value.toLowerCase());
-        month_value = months_in_eng[index-1];
+        let index = monthsInEng.indexOf(monthValue.toLowerCase());
+        monthValue = monthsInEng[index-1];
         }
     } else 
-        if (month_value.toLowerCase() == "chaitra"){
-            year_value += 1;
-            month_value = 'baishakh';
+        if (monthValue.toLowerCase() == "chaitra"){
+            yearValue += 1;
+            monthValue = 'baishakh';
         } else {
-        let index = months_in_eng.indexOf(month_value.toLowerCase());
-        month_value = months_in_eng[index+1];
+        let index = monthsInEng.indexOf(monthValue.toLowerCase());
+        monthValue = monthsInEng[index+1];
         }
     
-    month_element.innerText = month_value;
-    year_element.innerText = year_value;
+    monthElement.innerText = monthValue;
+    yearElement.innerText = yearValue;
 
-    if (current_date_td_element != null)
-    current_date_td_element.setAttribute("style", "");
-    showCalendar(month_to_index[capitalizeString(month_value)], year_value);
+    if (currentDateTdElement != null)
+    currentDateTdElement.setAttribute("style", "");
+    showCalendar(monthToIndex[capitalizeString(monthValue)], yearValue);
 }
 
-function populateWeekdays(element, array_list){
+function populateWeekdays(element, arrayList){
 
     element.setAttribute("style", "font-size:10px;")
-    let tr_element = document.createElement("tr");
-    for (let item of array_list){
-        let th_element = document.createElement("th");
-        th_element.setAttribute("id", item);
-        th_element.innerText = item.slice(0,3);
-        tr_element.appendChild(th_element);
+    let trElement = document.createElement("tr");
+    for (let item of arrayList){
+        let thElement = document.createElement("th");
+        thElement.setAttribute("id", item);
+        thElement.innerText = item.slice(0,3);
+        trElement.appendChild(thElement);
     }
-    element.append(tr_element);
+    element.append(trElement);
 }
 
-function create_tr_values(){
+function createTrValues(){
     let tbody = document.querySelector("tbody");
     for (let i=0; i < 5; i++){
-        let tr_element = document.createElement("tr");
+        let trElement = document.createElement("tr");
         for (let i=0; i < 7; i++){
-            let td_element = document.createElement("td");
-            td_element.addEventListener("click",
-             ()=>{ displayDayInformation(td_element)})
-            tr_element.appendChild(td_element);
+            let tdElement = document.createElement("td");
+            tdElement.addEventListener("click",
+             ()=>{ displayDayInformation(tdElement)})
+            trElement.appendChild(tdElement);
         }
-        tbody.append(tr_element);
+        tbody.append(trElement);
     }
 }
 
-function displayDayInformation(td_element){
-    let date = td_element.id.split('-');
+function displayDayInformation(TdElement){
+    let date = TdElement.id.split('-');
     let year = date[0];
     let month = date[1];
     let day = date[2];
-    let all_data = yearData["months"][capitalizeString(index_to_month[month-1])]["days"][day-1];
+    let allData = yearData["months"][capitalizeString(indexToMonth[month-1])]["days"][day-1];
 
-    all_display_spans = document.querySelectorAll(".display");
+    allDisplaySpans = document.querySelectorAll(".display");
     
-    for (let span_element of all_display_spans){
-        span_element.innerText = all_data[span_element.id]
+    for (let spanElement of allDisplaySpans){
+        spanElement.innerText = allData[spanElement.id]
     }
     document.querySelector("#month_display").innerText = document.querySelector("#current_month").innerText;
     document.querySelector("#year_display").innerText = document.querySelector("#current_year").innerText;
         // need to do for year display and month display
-    if (current_selected_td_element_id != `${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`){
-        current_selected_td_element.setAttribute("style", "");}
+    if (currentSelectedTdElementID != `${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`){
+        currentSelectedTdElement.setAttribute("style", "");}
 
-    current_selected_td_element = td_element;
-    current_selected_td_element_id = td_element.getAttribute('id');
+    currentSelectedTdElement = TdElement;
+    currentSelectedTdElementID = TdElement.getAttribute('id');
 
-    if (current_selected_td_element_id == `${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`){
-            current_selected_td_element.setAttribute("style", "border-radius: 50%;background-color:blue;line-height: 100%;text-align: center;");
+    if (currentSelectedTdElementID == `${currentNepaliDate.getYear()}-${currentNepaliDate.getMonth()+1}-${currentNepaliDate.getDate()}`){
+            currentSelectedTdElement.setAttribute("style", "border-radius: 50%;background-color:blue;line-height: 100%;text-align: center;");
     } else {
-        current_selected_td_element.setAttribute("style", "border-radius: 50%;background-color:orange;line-height: 100%;text-align: center;");
+        currentSelectedTdElement.setAttribute("style", "border-radius: 50%;background-color:orange;line-height: 100%;text-align: center;");
 }
 }
 
 window.onload = function (){
 
     populateWeekdays(document.querySelector("thead"),weekNamesArray);
-    let prev_element = document.querySelector(".prev");
-    let next_element = document.querySelector(".next");
-    prev_element.addEventListener("click", ()=>{changeMonthAndYear("previous");});
-    next_element.addEventListener("click", ()=>{changeMonthAndYear("next");});
+    let prevElement = document.querySelector(".prev");
+    let nextElement = document.querySelector(".next");
+    prevElement.addEventListener("click", ()=>{changeMonthAndYear("previous");});
+    nextElement.addEventListener("click", ()=>{changeMonthAndYear("next");});
 
-    let month_value = currentNepaliDate.getMonth();
-    let year_value = currentNepaliDate.getYear();
+    let monthValue = currentNepaliDate.getMonth();
+    let yearValue = currentNepaliDate.getYear();
 
-    document.querySelector("#current_month").innerText = index_to_month[month_value];
-    document.querySelector("#current_year").innerText = year_value;
+    document.querySelector("#current_month").innerText = indexToMonth[monthValue];
+    document.querySelector("#current_year").innerText = yearValue;
 
-    create_tr_values();
-    showCalendar(month_value, year_value, true);
+    createTrValues();
+    showCalendar(monthValue, yearValue, true);
 }
